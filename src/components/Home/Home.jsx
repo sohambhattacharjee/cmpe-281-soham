1c1ac9286020a4cad8d88fe0ec7f51c8ed3a3ec7
Soham Bhattacharjee <soham.onlyn@gmail.com>
2021-10-01 23:57:06 -0700

    pretty home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,32 +1,39 @@
 import React, { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import { ACCESS_TOKEN_NAME, API_BASE_URL } from '../../constants/apiConstants';
-import axios from 'axios'
+import axios from 'axios';
+import './Home.css';
+
 function Home(props) {
-    useEffect(() => {
-        try {
-            const accessToken = JSON.parse(localStorage.getItem(ACCESS_TOKEN_NAME))
-            
-        }
-        catch (ex) {
-            redirectToLogin()
-        }
-        // axios.get(API_BASE_URL, { headers: { 'token': localStorage.getItem(ACCESS_TOKEN_NAME) }})
-        // .then(function (response) {
-        //     if(response.status !== 200){
-        //       redirectToLogin()
-        //     }
-        // })
-        // .catch(function (error) {
-        //   redirectToLogin()
-        // });
-    })
+
     function redirectToLogin() {
         props.history.push('/login');
     }
+
+    function openFileDrawer() {
+        document.getElementById('uploadFile').click()
+    }
+    function handleUpload(e) {
+
+    }
     return (
-        <div className="mt-2">
-            Home page content
+        <div>
+            <div className="file-card">
+                <div className="alert-tex">
+                    Looks like you dont have any files yet!!
+                </div>
+                <div className="btn-wrapper">
+                    <input id='uploadFile' type='file' onChange={handleUpload} hidden />
+                    <button
+                        type="button"
+                        className="btn-primary"
+                        onClick={openFileDrawer}
+                    >
+                        Upload
+                    </button>
+                    <div className="message-dec">**Upto 10 Mb</div>
+                </div>
+            </div>
         </div>
     )
 }
